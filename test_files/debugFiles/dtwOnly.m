d86350e460e7f4a0a425cb278c4b2b3d4e0f0548
Johanna Devaney <jcdevaney@gmail.com>
2024-07-15 08:58:23 -0400

    debugging files

---
+++ test_files/debugFiles/dtwOnly.m
@@ -0,0 +1,42 @@
+% audiofile=('example.wav');
+% midifile=('example.mid');
+
+% audiofile = 'polyExample.wav';
+% midifile = 'polyExample.mid';
+
+audiofile=('Monteverdi_A un giro sol.wav');
+midifile=('/Users/jcdevaney/Desktop/pyAMPACTtest/Monteverdi-A un giro sol Bb 2v.midi');
+
+% audiofile=('Monteverdi_ Cruda Amarilli.wav');
+% midifile=('/Users/jcdevaney/Desktop/pyAMPACTtest/Monteverdi-Cruda Amarilli-4v.midi');
+
+width = 3;
+targetsr = 4000;
+nharm = 3; 
+winms = 100; 
+hop = 32;
+
+% % run the dyanamic time warping alignment
+[res,spec,dtw] = runDTWAlignment(audiofile, midifile, 0.025,width,targetsr,nharm,winms);
+
+
+% % HMM parameters
+% meansCovarsMat='polySingingMeansCovars.mat';
+% 
+% % % voice type for each polyphonic line
+
+% % % align MIDI to audio
+% [estimatedOns, estimatedOffs, nmat,dtw]=runPolyAlignment(audiofile, midifile, meansCovarsMat, voiceType);
+% 
+% % % for
+% for v = 1 : 2
+%     ons=nonzeros(estimatedOns{v});
+%     offs=nonzeros(estimatedOffs{v});
+%     loc=1;
+%     n = 1 
+%     % Estimate f0 for a matrix (or vector) of amplitudes and frequencies
+%     [f0{v}{loc}, pwr{v}{loc}, t{v}{loc}, M{v}{loc}, xf{v}{loc}] = f0EstWeightedSumSpec(audiofile, ons(loc), offs(loc), nmat{v}(n,4));
+%     % Estimate note-wise perceptual values
+%     noteVals{v}{loc}=estimatePerceptualParamters([f0{v}{loc}],[pwr{v}{loc}],[xf{v}{loc}],[M{v}{loc}],sr,256,1,sig);
+%     loc=loc+1;
+% end
